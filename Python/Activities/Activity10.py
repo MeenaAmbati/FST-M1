7f96a9b2bddbd7ad1d9e97d359474371912c170f
MeenaAmbati <101171844+MeenaAmbati@users.noreply.github.com>
2022-03-14 10:48:12 +0530

    Commit for Activity10

---
+++ Python/Activities/Activity10.py
@@ -0,0 +1,8 @@
+
+num_tuple = (15, 30, 23, 36, 50)
+print("Given list is ", num_tuple)
+
+print("Elements that are divisible by 5:")
+for num in num_tuple:
+    if (num % 5 == 0):
+        print(num)
