bd8b033e92ad901e6f7d0c9608a385a3136ced02
MeenaAmbati <101171844+MeenaAmbati@users.noreply.github.com>
2022-03-28 12:13:15 +0530

    Update Activity7_ReadingAdditionalInformation.java

--- Selenium/Project/Activity7_ReadingAdditionalInformation.java
+++ Selenium/Project/Activity7_ReadingAdditionalInformation.java
@@ -14,7 +14,7 @@ public class Activity7_ReadingAdditionalInformation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\GrandhiLavanya\\Downloads\\chromedriver_win89\\chromedriver.exe");
+				"C:\\Users\\MeenaAmbati\\Downloads\\chromedriver_win89\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://alchemy.hguy.co/crm/index.php?action=Login&module=Users");
 		driver.manage().window().maximize();
