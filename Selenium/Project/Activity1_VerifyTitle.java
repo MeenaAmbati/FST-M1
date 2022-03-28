9ac1628cab328940151f556329811778d3e6fe0e
MeenaAmbati <101171844+MeenaAmbati@users.noreply.github.com>
2022-03-28 12:04:46 +0530

    Update Activity1_VerifyTitle.java

--- Selenium/Project/Activity1_VerifyTitle.java
+++ Selenium/Project/Activity1_VerifyTitle.java
@@ -11,7 +11,7 @@ public class Activity1_VerifyTitle {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\GrandhiLavanya\\Downloads\\chromedriver_win89\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Meena Ambati\\Downloads\\chromedriver_win89\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://alchemy.hguy.co/crm/index.php?action=Login&module=Users");
 		driver.manage().window().maximize();
