a86065a475790b2a319a383cff3d16dc7b152ed6
MeenaAmbati <101171844+MeenaAmbati@users.noreply.github.com>
2022-03-28 12:11:56 +0530

    Update Activity6_VerifyActivityMenu.java

--- Selenium/Project/Activity6_VerifyActivityMenu.java
+++ Selenium/Project/Activity6_VerifyActivityMenu.java
@@ -9,7 +9,7 @@ public class Activity6_VerifyActivityMenu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\GrandhiLavanya\\Downloads\\chromedriver_win89\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\MeenaAmbati\\Downloads\\chromedriver_win89\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://alchemy.hguy.co/crm/index.php?action=Login&module=Users");
 		driver.manage().window().maximize();
